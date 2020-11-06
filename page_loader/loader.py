14e3222cf94115cd87df800efc62ab7ab7606ef9
Aleksey94Dan <50262563+Aleksey94Dan@users.noreply.github.com>
2020-11-06 12:07:35 +0300

    Update loader.py

--- page_loader/loader.py
+++ page_loader/loader.py
@@ -5,7 +5,8 @@
 import os
 import re
 from urllib.parse import urlparse
-
+from bs4 import BeautifulSoup, SoupStrainer
+from pprint import pprint
 import requests
 
 USER_AGENT = (
@@ -13,23 +14,26 @@ USER_AGENT = (
     ' (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'  # noqa: WPS326
 )
 
-EXTENSION = '.html'
 
-def get_name(url: str, replacement_sign: str = '-') -> str:
+EXTENSION: str = '.html'
+REPLACEMENT_SIGN: str = '-'
+
+
+def get_path_to_file(url: str, *, directory: str = '') -> str:
     """Return the transformed name by pattern."""
     parsed_url = urlparse(url)
     domain = '{}{}'.format(parsed_url.netloc, parsed_url.path)
     pattern = re.compile(r'\W|_')
-    return '{}{}'.format(re.sub(pattern, replacement_sign, domain), EXTENSION)
+    directory = directory
+    name = '{}{}'.format(re.sub(pattern, REPLACEMENT_SIGN, domain), EXTENSION)
+    return os.path.join(directory, name)
 
 
-def load(url: str, directory: str = '') -> None:
+def load(url: str, *, path_to_file: str = '') -> None:
     """Download html document to the specified directory."""
     headers = {'user-agent': USER_AGENT}
     response = requests.get(url, headers=headers)
     if response.status_code == requests.codes.all_ok:
-        name = get_name(url)
-        path_to_file = os.path.join(directory, name)
         if response.encoding:
             with open(path_to_file, 'w') as html:
                 html.write(response.text)
@@ -38,3 +42,30 @@ def load(url: str, directory: str = '') -> None:
                 html.write(response.content)
     else:
         response.raise_for_status()
+
+
+def has_src(string):
+    return string is not None and 'src' in string
+
+
+def get_resourse(path_to_file: str) -> None:
+    with open(path_to_file) as html:
+        html = html.read()
+
+    # only_links = SoupStrainer('link', atrr)
+    only_scripts = SoupStrainer('script')
+    # only_img = SoupStrainer('script')
+    soup = BeautifulSoup(html, "html.parser", parse_only=only_scripts)  # for windows
+    print(soup.prettify())
+    # soup = BeautifulSoup(html, "lxml")  # for linux
+    # links = soup.find_all('link')
+    # scripts = soup.find_all('script')
+    # imgs = soup.find_all('img')
+    # pprint(links, scripts, imgs)
+
+
+if __name__ == "__main__":
+    url = 'https://hexlet.io/courses'
+    path_to_file = get_path_to_file(url)
+    load(url, path_to_file=path_to_file)
+    get_resourse(path_to_file)
