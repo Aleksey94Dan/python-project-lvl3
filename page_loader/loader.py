ad96eae5c0b8275d39619cd040fa6c03cc0ac7e0
Aleksey Danilchenko <danilchenko.aleksey94@gmail.com>
2020-11-20 00:04:42 +0700

    update

--- page_loader/loader.py
+++ page_loader/loader.py
@@ -2,101 +2,44 @@
 
 """The module transforms the url and loads the html."""
 
-import os
+
+from urllib.parse import urlparse, unquote
 import re
-from urllib.parse import urlparse
-from bs4 import BeautifulSoup, SoupStrainer
-from pprint import pprint
-import requests
-from pprint import pprint
+import os
 
 
-USER_AGENT = (
-    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36'
-    ' (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'  # noqa: WPS326
-)
-
+ATRIBUTES = ('scheme', 'netloc')
 REPLACEMENT_SIGN = '-'
-TAGS = 'link|script|img'
+EXTENSION = '.html'
+BASE62 = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
 
 
-def get_name(url: str) -> str:
-    """Return the transformed name by pattern."""
+
+
+
+
+
+
+
+@make_short_name
+def get_name_from_url(url: str) -> str:
+    length_url = len(url)
+    if not url:
+        raise ValueError('Missing URL!')
+
+    if length_url > 2000:
+        raise ValueError(
+    'Your URL has passed the actual limit of 2000 characters.'
+    )
+
+    url = unquote(url)
     parsed_url = urlparse(url)
-    domain = '{}{}'.format(parsed_url.netloc, parsed_url.path)
-    domain, extension = os.path.splitext(domain)
-    if extension == '':
-        extension = '.html'
+
+    if not all((getattr(parsed_url, atribute) for atribute in ATRIBUTES)):
+        raise ValueError("'{}' string has not scheme or netloc".format(url))
+
+    domain = '{0}{1}'.format(parsed_url.netloc, parsed_url.path)
     pattern = re.compile(r'\W|_')
     name = re.sub(pattern, REPLACEMENT_SIGN, domain)
-    return '{}{}'.format(name, extension)
+    return '{0}{1}'.format(name, EXTENSION)
 
-
-def has_domain(url):
-    if url is None:
-        return False
-    parse_uri = urlparse(url)
-    if parse_uri.netloc:
-        return True
-    return False
-
-def get_directory(name: str, *, directory: str = ''):
-    name, extension = os.path.splitext(name)
-    if extension == '.html':
-        name = '{}{}'.format(name, '_files')
-    path_to_dir = os.path.join(directory, name)
-    if os.path.exists(path_to_dir):
-        return path_to_dir
-    else:
-        os.makedirs(path_to_dir)
-        return path_to_dir
-
-
-
-def load(url: str, *, directory: str = '') -> None:
-    """Download html document to the specified directory."""
-    headers = {'user-agent': USER_AGENT}
-    response = requests.get(url, headers=headers)
-    if response.status_code == requests.codes.all_ok:
-        if response.encoding:
-            name = get_name(url)
-            path_to_file = os.path.join(directory, name)
-            with open(path_to_file, 'w') as html:
-                html.write(response.text)
-        else:
-            with open(path_to_file, 'wb') as html:
-                html.write(response.content)
-    response.raise_for_status()
-
-
-def get_url_resourse(path_to_file: str) -> None:
-    with open(path_to_file) as html:
-        html = html.read()
-    only_tags = SoupStrainer(re.compile(TAGS))
-    soup = BeautifulSoup(html, "lxml", parse_only=only_tags)
-    sripts = soup.find_all('script')
-    links = soup.find_all('link')
-    imgs = soup.find_all('img')
-    urls = []
-    for sript in sripts:
-        urls.append(sript.get('src'))
-    for link in links:
-        urls.append(link.get('href'))
-    for img in imgs:
-        urls.append(img.get('href'))
-    urls = list(filter(has_domain, urls))
-    return urls
-
-
-
-if __name__ == "__main__":
-    url = 'https://hexlet.io/courses'
-    name = get_name(url)
-    path_to_save = 'abc'
-    load(url, directory=path_to_save)
-    directory_for_locale = get_directory(name, directory=path_to_save)
-    print(directory_for_locale)
-
-    # print(name)
-    # directory = os.path.join(path_to_save, get_directory(name))
-    # print(directory)
--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -5,26 +5,25 @@
 import re
 import pytest
 
-ACTUAL_URL = 'path_to_file'
+from page_loader import loader
 
-def test_get_name_from_url():
+with open('tests/fixture/actual_urls') as urls:
+    ACTUAL_URLS = urls.read().strip().splitlines()
 
-    with open(ACTUAL_URL) as url:
-        actual = url.read().strip().split()
 
-    actuals = map(get_name_from_url, actual)
-    expected = re.compile(r'^[\w-]+\.html')
-    
-    for actual in actuals:
-        assert actual == expected, "URL does not match pattern"
-    
-http://www.example.com/index.html
-https://ru.hexlet.io/courses
-https://en.wikipedia.org/wiki/URL
-https://aliexpress.ru/home.htm
-https://campaign.aliexpress.com/wow/gcp/ae/channel/ae/accelerate/tupr?spm=a2g0o.home.15027.1.17ba5c98ckbHwJ&wh_pid=ae/mega/ae/2020_super_friday/shoes&wh_weex=true&_immersiveMode=true&wx_navbar_hidden=true&wx_navbar_transparent=true&ignoreNavigationBar=true&wx_statusbar_hidden=true&gps-id=300000000564511&productIds=4001132140041
-https://admiralmarkets.com/ru/education/articles/trading-instruments/index-sp500-trading
-https://disenowebakus.net/imagenes/logo-akus.jpg
-https://developer.mozilla.org/en-US/docs/Learn
-https://aliexpress.ru/item/32813937749.html?spm=a2g0o.detail.1000014.8.4b7613fewRhqry&gps-id=pcDetailBottomMoreOtherSeller&scm=1007.14976.157518.0&scm_id=1007.14976.157518.0&scm-url=1007.14976.157518.0&pvid=9db37213-0a22-456a-bb4e-c15adc595ecf&_t=gps-id:pcDetailBottomMoreOtherSeller,scm-url:1007.14976.157518.0,pvid:9db37213-0a22-456a-bb4e-c15adc595ecf,tpp_buckets:668%230%23131923%2358_668%23808%236395%23442_668%23888%233325%2319_4976%230%23157518%230_4976%232711%237538%23324_4976%233223%2310815%238_4976%233104%239653%237_4976%233141%239887%231_668%232846%238111%23489_668%232717%237566%23859_&_ga=2.195680741.859224365.1594002038-431412820.1580367853
-https://www.windy.com/ru/-%D0%A0%D0%9C2-5-pm2p5?cams,pm2p5,55.332,86.054,11a
+
+@pytest.mark.parametrize('url', ACTUAL_URLS)
+def test_get_name_from_url(url):
+    expected_pattern = re.compile(r'^[\w-]+\.html')
+    assert re.fullmatch(expected_pattern, loader.get_name_from_url(url))
+
+
+@pytest.mark.parametrize('url, message', [
+    ('www.example.com/index.html', "'www.example.com/index.html' string has not scheme or netloc"),
+    ('/index', "'/index' string has not scheme or netloc"),
+    ('', "Missing URL!"),
+    ('www.example.com/' + '/a'*2000, 'Your URL has passed the actual limit of 2000 characters.')
+])
+def test_get_name_from_url_exception(url, message):
+    with pytest.raises(ValueError, match=message):
+        loader.get_name_from_url(url)
