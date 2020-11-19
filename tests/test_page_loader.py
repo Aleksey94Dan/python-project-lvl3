b8a6817006e1d6c775bf2f7d5d51f22beb172b99
Aleksey94Dan <50262563+Aleksey94Dan@users.noreply.github.com>
2020-11-19 12:36:40 +0300

    Update test_page_loader.py

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -2,31 +2,22 @@
 
 """Testing all modules page-loader."""
 
-import os
 import re
-import tempfile
-
 import pytest
 
-from page_loader import loader
-
-URL = r"https://hexlet.io/courses"
-
-@pytest.mark.parametrize("url", [URL])
-def test_name(url):
-    """Testing a filename against a pattern."""
-    pattern = r'([a-zA-Z0-9-]+)((\.html)|(_files))'
-    name_file = loader.get_name(url)
-    name_directory = loader.get_name(url)
-    assert re.fullmatch(pattern, name_file)
+ACTUAL_URL = 'path_to_file'
+EXPECTED_URL = 'path_to_file'
 
 
-@pytest.mark.parametrize("url", [URL])
-def test_load_page(url):
-    """Test of page load"""
-    with tempfile.TemporaryDirectory() as tmpdirname:
-        name_file = loader.get_name(url)
-        path_to_file = os.path.join(tmpdirname, name_file)
-        loader.load(url, directory=tmpdirname)
-        assert os.path.isfile(path_to_file)
+def test_get_name_from_url():
 
+    with open(ACTUAL_URL) as url:
+        actual = url.read().strip().split()
+
+    with open(EXPECTED_URL) as url:
+        expected = url.read().strip().split()
+
+    actual = sorted(map(get_name_from_url, actual))
+    expected = sorted(expected)
+
+    assert actual == expected
