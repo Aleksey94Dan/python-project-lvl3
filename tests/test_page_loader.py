d006556662f0b7a63acf5f415a7050d45cfc824a
Aleksey94Dan <50262563+Aleksey94Dan@users.noreply.github.com>
2020-11-19 12:56:28 +0300

    Update test_page_loader.py

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -6,18 +6,15 @@ import re
 import pytest
 
 ACTUAL_URL = 'path_to_file'
-EXPECTED_URL = 'path_to_file'
-
 
 def test_get_name_from_url():
 
     with open(ACTUAL_URL) as url:
         actual = url.read().strip().split()
 
-    with open(EXPECTED_URL) as url:
-        expected = url.read().strip().split()
-
-    actual = sorted(map(get_name_from_url, actual))
-    expected = sorted(expected)
-
-    assert actual == expected
+    actuals = map(get_name_from_url, actual)
+    expected = re.compile(r'^[\w-]+\.html')
+    
+    for actual in actuals:
+        assert actual == expected, "URL does not match pattern"
+    
