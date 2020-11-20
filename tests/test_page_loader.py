13f9f879e61bbeabfc2bc46fc40771df373525d8
Aleksey94Dan <50262563+Aleksey94Dan@users.noreply.github.com>
2020-11-20 06:03:59 +0300

    Update test_page_loader.py

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -6,6 +6,9 @@ import re
 import pytest
 
 from page_loader import loader
+from pytest_localserver.http import WSGIServer
+
+PATH_TO_HTML = 'path_to_file'
 
 with open('tests/fixture/actual_urls') as urls:
     ACTUAL_URLS = urls.read().strip().splitlines()
@@ -27,3 +30,30 @@ def test_get_name_from_url(url):
 def test_get_name_from_url_exception(url, message):
     with pytest.raises(ValueError, match=message):
         loader.get_name_from_url(url)
+
+        
+def app(environ, start_reponse):
+    """Simple test application."""
+
+    status = '200 OK'
+    response_headers = ['Content-type', 'text/html']
+
+    with open(PATH_TO_HTML) as f:
+        html = f.read()
+    start_reponse(status, response_headers)
+    return [html.encode('utf-8')]
+
+
+@pytest.fixture
+def testserver(request):
+    """Defines the testserver funcarg."""
+    server = WSGIServer(application=app)
+    server.start()
+    request.addfinalizer(testserver)
+    return server
+
+
+def test_retrieve_some_content(testserver):
+    with open(PATH_TO_HTML) as f:
+        expected = f.read()
+    assert loader.scrape(testserver.url) == expected.encode('utf-8')
