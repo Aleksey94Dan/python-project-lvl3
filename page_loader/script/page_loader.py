0d61a09669ab0c6f9a1f0d7e79ee01e06776a0e2
Aleksey94Dan <danilchenko.aleksey94@gmail.com>
2021-01-31 17:18:08 +0700

    update log config

--- page_loader/script/page_loader.py
+++ page_loader/script/page_loader.py
@@ -4,35 +4,38 @@
 
 """The main parsing script."""
 
+import logging
 import sys
 
 from requests import exceptions
 
-from page_loader import cli, loader, logging_app
+from page_loader import cli, loader
 
 START = 'Start'
-FINISH = 'Finish'
+FINISH = 'The program ended successfully'
 SYSTEM_EXIT_ABORT = 1
 
 
 def main() -> None:
     """Run a code."""
-    logger = logging_app.logger
     path_to_file, url, verbose = cli.parse()
-
-    logger_level = logger.debug if verbose else logger.info
+    logger_level = logging.DEBUG if verbose else logging.INFO
+    logging.basicConfig(
+        format='%(asctime)s - %(levelname)s - %(message)s',  # noqa: WPS323
+        level=logger_level,
+    )
+    logger = logging.getLogger(__name__)
     logger.info(START)
     try:
         loader.download(url, path_to_file)
     except (TypeError, exceptions.MissingSchema, exceptions.InvalidSchema):
-        logger_level(
+        logger.error(
             SystemExit('Please enter correct URL. Exit code {0}.'.format(
                 SYSTEM_EXIT_ABORT,
             ),
             ),
         )
         sys.exit(SYSTEM_EXIT_ABORT)
-    logger.info('The program ended successfully')
     logger.info(FINISH)
 
 
