f3fe1dc6b8eb1136b8200447361588b4687aed4d
Aleksey94Dan <50262563+Aleksey94Dan@users.noreply.github.com>
2021-02-10 13:05:43 +0300

    Update page_loader.py
    deleted difucult log

--- page_loader/script/page_loader.py
+++ page_loader/script/page_loader.py
@@ -7,39 +7,17 @@
 import logging
 import sys
 
-from requests import exceptions
+from page_loader import cli
+from page_loader import loader
+from page_loader import logging as my_logging
 
-from page_loader import cli, loader
-
-START = 'Start'
-FINISH = 'The program ended successfully'
-SYSTEM_EXIT_ABORT = 1
-SYSTEM_EXIT_GOOD = 0
+ABORT_CODE = 1
+OK_CODE = 0
 
 
 def main() -> None:  # noqa: WPS210
     """Run a code."""
-    path_to_file, url, verbose = cli.parse()
-    logger_level = logging.DEBUG if verbose else logging.INFO
-    logging.basicConfig(
-        format='%(asctime)s - %(levelname)s - %(message)s',  # noqa: WPS323
-        level=logger_level,
-    )
-    logger = logging.getLogger(__name__)
-    logger.info(START)
-    exit_code = SYSTEM_EXIT_GOOD
-    try:
-        loader.download(url, path_to_file)
-    except (TypeError, exceptions.MissingSchema, exceptions.InvalidSchema):
-        logger.error(
-            SystemExit('Please enter correct URL. Exit code {0}.'.format(
-                SYSTEM_EXIT_ABORT,
-            ),
-            ),
-        )
-        exit_code = SYSTEM_EXIT_ABORT
-    sys.exit(exit_code)
-    logger.info(FINISH)
+    args = cli.get_parser().parse_args()
 
 
 if __name__ == '__main__':
