94a5a18d921bd6b6db8d203298569c535a1f0550
Aleksey94Dan <50262563+Aleksey94Dan@users.noreply.github.com>
2021-02-20 07:58:07 +0300

    Create errors.py
    add errors

---
+++ page_loader/errors.py
@@ -0,0 +1,19 @@
+# -*- coding:utf-8 -*-
+
+"""The module handles errors."""
+
+
+class DownloadError(Exception):
+    """Exception due to download errors."""
+    def __init__(self, expression, message):
+        """Initializate of starting accepted values."""
+        self.expression = expression
+        self.message = message
+
+
+class DownloadNetworkError(DownloadError):
+    """Connection error exception."""
+    def __init__(self, expression, message):
+        """Initializate of starting accepted values."""
+        self.expression = expression
+        self.message = message
