6164412c3dbd1eb2326c5a91a72be8df2ec5f6f9
Aleksey94Dan <50262563+Aleksey94Dan@users.noreply.github.com>
2021-02-26 12:59:18 +0300

    Update scrape.py
    del comment

--- page_loader/scrape.py
+++ page_loader/scrape.py
@@ -17,21 +17,9 @@ def get_content(url: str) -> Union[str, bytes]:
     try:
         response = requests.get(url)
         return response.text if response.encoding else response.content
-    except InvalidURL as err:
-        raise InvalidURL('bls')
-        # print(err)
-        # print(err.args)
-        # print(err.__cause__)
-        # print(err.__context__)
-    except MissingSchema as err:
-        raise MissingSchema('ljl')
-        # print(err)
-        # print(err.args)
-        # print(err.__cause__)
-        # print(err.__context__)
-    except InvalidSchema as err:
-        raise InvalidSchema('asdfj')
-        # print(err)
-        # print(err.args)
-        # print(err.__cause__)
-        # print(err.__context__)
+    except InvalidURL as err1:
+        raise InvalidURL()
+    except MissingSchema as err2:
+        raise MissingSchema()
+    except InvalidSchema as err3:
+        raise InvalidSchema()
